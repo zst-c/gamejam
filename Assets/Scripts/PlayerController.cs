cb251aed9ba28513ab3ae44abbe1fbb2f052fa0d
l-Luna <izzeldeen03@gmail.com>
2022-11-02 14:24:39 +0000

    clenup playercontroller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,19 +1,15 @@
-// using System.Collections;
-
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // [SerializeField] float timeToMoveBetweenTheSpacesInTheGrid = 0.2f;
-    // Vector2 originPosition, targetPosition;
-    // bool isMoving;
-
     [SerializeField] float playerMovementSpeed = 5f;
     [SerializeField] Transform movePoint;
     [SerializeField] LayerMask whatLayerStopsMovement;
 
     [SerializeField] GameMemory memory;
+
+    // index into game memory where x and y are stored
+    private const int xPtr = 0, yPtr = 1;
 
     void Start()
     {
@@ -22,42 +18,13 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        // Bro what the fuck is happening, it works, but just sometimes
-        // Vector2 moveInput = new(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        // if (!isMoving && Input.GetAxis("Vertical") != 0 || Input.GetAxis("Horizontal") != 0)
-        // {
-        //     StartCoroutine(MovePlayer(moveInput));
-        // }
-
-        // Scuff implementation but it works I guess
-        // if (!isMoving)
-        // {
-        //     if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
-        //     {
-        //         StartCoroutine(MovePlayer(Vector2.up));
-        //     }
-        //     if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
-        //     {
-        //         StartCoroutine(MovePlayer(Vector2.left));
-        //     }
-        //     if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
-        //     {
-        //         StartCoroutine(MovePlayer(Vector2.down));
-        //     }
-        //     if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-        //     {
-        //         StartCoroutine(MovePlayer(Vector2.right));
-        //     }
-        // }
-
         transform.position = Vector3.MoveTowards(transform.position, movePoint.position, playerMovementSpeed * Time.deltaTime);
 
         if (Vector3.Distance(transform.position, movePoint.position) <= 0.05f)
         {
-            // temporary
             memory.ChangeMemory(() => {
-                memory.memory[0] = (byte)transform.position.x;
-                memory.memory[1] = (byte)transform.position.y;
+                memory.memory[xPtr] = (byte)transform.position.x;
+                memory.memory[yPtr] = (byte)transform.position.y;
             });
             if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1)
             {
@@ -75,20 +42,4 @@ public class PlayerController : MonoBehaviour
             }
         }
     }
-
-    // IEnumerator MovePlayer(Vector2 direction)
-    // {
-    //     isMoving = true;
-    //     float elapsedTime = 0;
-    //     originPosition = transform.position;
-    //     targetPosition = originPosition + direction;
-    //     while (elapsedTime < timeToMoveBetweenTheSpacesInTheGrid)
-    //     {
-    //         transform.position = Vector2.Lerp(originPosition, targetPosition, (elapsedTime / timeToMoveBetweenTheSpacesInTheGrid));
-    //         elapsedTime += Time.deltaTime;
-    //         yield return null;
-    //     }
-    //     transform.position = targetPosition;
-    //     isMoving = false;
-    // }
 }
