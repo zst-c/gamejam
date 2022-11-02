03458f525f0a0abda96aa7ec570487fdb06b64c2
miloszwasacz <miloszwasacz@gmail.com>
2022-11-02 14:29:16 +0000

    Add basic Settings screen

---
+++ Assets/Scripts/SettingsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsController : MonoBehaviour
+{
+    private int _brightness;
+    public Text brightness;
+
+    public void Update()
+    {
+        brightness.text = $"{_brightness}/100";
+    }
+
+    public void SetBrightness() {}
+}
