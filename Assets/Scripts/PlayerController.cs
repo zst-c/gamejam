be5daee1e36c4fd67ae04c7fb0ed5dcbe0e027b0
Chifunt <chifunt@gmail.com>
2022-11-02 10:21:57 +0000

    Player Controller works now

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,16 +14,36 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        Vector2 moveInput = new(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        if (!isMoving && Input.GetAxis("Vertical") != 0 || Input.GetAxis("Horizontal") != 0)
+        // Bro what the fuck is happening, it works, but just sometimes
+        // Vector2 moveInput = new(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        // if (!isMoving && Input.GetAxis("Vertical") != 0 || Input.GetAxis("Horizontal") != 0)
+        // {
+        //     StartCoroutine(MovePlayer(moveInput));
+        // }
+        if (!isMoving)
         {
-            StartCoroutine(MovePlayer(moveInput));
+            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+            {
+                StartCoroutine(MovePlayer(Vector2.up));
+            }
+            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            {
+                StartCoroutine(MovePlayer(Vector2.left));
+            }
+            if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+            {
+                StartCoroutine(MovePlayer(Vector2.down));
+            }
+            if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            {
+                StartCoroutine(MovePlayer(Vector2.right));
+            }
         }
     }
 
     void FixedUpdate()
     {
-        
+
     }
 
     IEnumerator MovePlayer(Vector2 direction)
