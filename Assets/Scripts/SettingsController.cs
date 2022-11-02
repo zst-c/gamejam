475cb5bd64983d960a7f447fb7a90c0a07715687
miloszwasacz <miloszwasacz@gmail.com>
2022-11-02 21:36:05 +0000

    Add selecting the default buttons

--- Assets/Scripts/SettingsController.cs
+++ Assets/Scripts/SettingsController.cs
@@ -10,8 +10,12 @@ public class SettingsController : MonoBehaviour
     public Slider brightnessSlider;
     public TextMeshProUGUI brightnessMeter;
 
+    public Button resumeButton;
+
     private void Start()
     {
+        resumeButton.Select();
+
         SettingsManager.Instance.Volume = (int)volumeSlider.value;
         volumeSlider.onValueChanged.AddListener(delegate(float value)
         {
