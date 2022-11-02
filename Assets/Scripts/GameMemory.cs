e37ebebbe19a4f07b283c5c8e3b9d18a9dae14a4
l-Luna <izzeldeen03@gmail.com>
2022-11-02 12:59:18 +0000

    commenting

--- Assets/Scripts/GameMemory.cs
+++ Assets/Scripts/GameMemory.cs
@@ -6,14 +6,18 @@ public class GameMemory : MonoBehaviour{
 	public byte[] memory = new byte[256];
 	public bool[] updated = new bool[256];
 
+	// Batches a number of changes to memory and tracks what cells were changed
 	public void ChangeMemory(Action action){
 		var oldMemory = new byte[256];
 		var oldChanged = new bool[256];
+		// keep track of the previous memory and highlighting
 		for(int i = 0; i < 256; i++){
 			oldMemory[i] = memory[i];
 			oldChanged[i] = updated[i];
 		}
+		// run batch changes
 		action();
+		// only update `updated` if anything was changed
 		bool commit = false;
 		for(var i = 0; i < 256; i++){
 			var changed = oldMemory[i] != memory[i];
@@ -22,6 +26,7 @@ public class GameMemory : MonoBehaviour{
 				commit = true;
 		}
 
+		// if nothing was changed, keep old highlighting
 		if(!commit)
 			updated = oldChanged;
 	}
