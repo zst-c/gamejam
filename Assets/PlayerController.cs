dc28ae76b95f33d5cbea5ce8d915fba77ff58f81
Chifunt <chifunt@gmail.com>
2022-11-02 09:46:57 +0000

    Add initial game files

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float timeToMoveBetweenTheSpacesInTheGrid = 0.2f;
+    Vector2 originPosition, targetPosition;
+    bool isMoving;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        Vector2 moveInput = new(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        if (!isMoving && Input.GetAxis("Vertical") != 0 || Input.GetAxis("Horizontal") != 0)
+        {
+            Debug.Log("MOOVE");
+        }
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+
+    IEnumerator MovePlayer(Vector2 direction)
+    {
+        isMoving = true;
+        float elapsedTime = 0;
+        originPosition = transform.position;
+        targetPosition = originPosition + direction;
+        while (elapsedTime < timeToMoveBetweenTheSpacesInTheGrid)
+        {
+            transform.position = Vector2.Lerp(originPosition, targetPosition, (elapsedTime / timeToMoveBetweenTheSpacesInTheGrid));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        transform.position = targetPosition;
+        isMoving = false;
+    }
+}
