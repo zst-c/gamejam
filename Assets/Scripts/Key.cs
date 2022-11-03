99d08016c526063ac523ed449aaf50dfc0fe8352
l-Luna <izzeldeen03@gmail.com>
2022-11-03 11:58:28 +0000

    Key and Unlockable

---
+++ Assets/Scripts/Key.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Key : MonoBehaviour{
+
+	public string Colour = "";
+	
+	public void CheckUnlock(){ // scuffed "collision"
+		var pos = gameObject.transform.position;
+		foreach(var unlockable in FindObjectsOfType<Unlockable>()){
+			if(unlockable.Colour == Colour && (unlockable.gameObject.transform.position - pos).magnitude < 0.5){
+				Destroy(unlockable.gameObject);
+				Destroy(gameObject);
+				break;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Unlockable.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+public class Unlockable : MonoBehaviour{
+
+	public string Colour = "";
+}
