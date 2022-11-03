ac9e56d90247852cd8f0d215b1d680ff06d50131
miloszwasacz <miloszwasacz@gmail.com>
2022-11-03 01:57:24 +0000

    Add generic Memory Backend

---
+++ Assets/Scripts/NewGameScene.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewGameScene : MonoBehaviour
+{
+    public GridLayoutGroup memoryGrid;
+    public TMP_FontAsset font;
+
+    private MemBackend _memory;
+
+    private void Start()
+    {
+        string[] labels = { "usnm [0]", "usnm [1]", "btn_Cc", "btn_Ok" };
+        _memory = new MemBackend(labels, memoryGrid, font);
+    }
+
+
+}
